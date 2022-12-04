cde68a8ab3068739beb74c6d085b80e922b0c8a0
valehgoja <qocayevvaleh96@gmail.com>
2022-12-04 20:06:33 +0100

    Loading animation

---
+++ script.js
@@ -0,0 +1,20 @@
+let percent = document.querySelector(".percent");
+let progress = document.querySelector(".progress");
+let textCompleted = document.querySelector(".text");
+
+let count = 4;
+let per = 16;
+let loading = setInterval(animate,50);
+function animate(){
+    if(count ==100 && per == 400){
+        percent.classList.add("text-blink");
+        textCompleted.style.display ="block";
+        clearInterval(loading);
+    }else{
+        per =per +4;
+        count =count+1;
+        progress.style.width = per + "px";
+        percent.textContent = count +"%";
+    }
+}
+
